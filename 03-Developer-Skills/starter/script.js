f0509d5635dc0d06b711ae491decd993a739836e
lei <littlesomerest@gmail.com>
2024-03-27 16:00:51 +0800

    complete coding challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
+const a = "a";
+const b = "b";
+console.log(a + b);
 
+// Code Chanllenge
+const printForecast = function (arr) {
+  let print = ``;
+  for (let i = 0; i < arr.length; i++) {
+    print += `${arr[i]}C in ${i + 1} days...`;
+  }
+  console.log("..." + print);
+};
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
